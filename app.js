4e50914814afc499b31bc09eda97f86c06fb3ac4
Natalia <natalia.levinta@amdaris.com>
2023-08-12 14:47:20 +0300

    first commit

--- app.js
+++ app.js
@@ -1,119 +1,136 @@
 import http from 'http'
-
-import  data from "./config.json" assert {type : "json"}
-
-import {cityName}  from './readline1.js'
-
-
+import data from "./config.json" assert {type: "json"}
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import { cityName, weatherDisplayed } from './readline1.js';
 
 
+const rl = readline.createInterface({ input, output });
 
 
 //HW: read city name from console
 //print to console temp min max, wind, ..
 //make it interactive
 // debug / prevent errors 
+// Ask a question and wait for user input
 
-const city = cityName;
-console.log(`!!!${city}`);
 
-const getCoordinates =  new Promise((resolve)=>{
+
+const getCoordinates = (city) => {
+    return new Promise((resolve) => {
+
         const callback = (res) => {
             //console.log(res);
             /// BIND EVENT HANDLERS
             let chunks = '';
-            res.on('end', ()=> {
+
+            res.on('end', () => {
                 const data = JSON.parse(chunks);
                 console.log(`!!!${chunks}`);
-                let {lon, lat}=data[0]
-                
-                 console.log(`!!!${lat }`);
-                 console.log(`!!!${lon }`);
-               resolve(data);
-    
+                let { lon, lat } = data[0]
+
+                console.log(`!!!${lat}`);
+                console.log(`!!!${lon}`);
+                resolve(data);
+
             })
-        
-            res.on('data', (chunk)=> {
+
+            res.on('data', (chunk) => {
                 chunks += chunk.toString();
-                console.log('API response with data');              
+                console.log('API response with data');
             })
-        
-            res.on('error', ()=> {
+
+            res.on('error', () => {
                 console.log('API response with error');
-                
+
             })
-        }   
-        
+        }
+
         // //preparerequest 
         const req = http.request({
             host: data.HOST,
-            path: data.PATH2 + `?q=${city}&appid=${data.KEY}`, 
+            path: data.PATH2 + `?q=${city}&appid=${data.KEY}`,
             method: 'GET',
             port: 80
         }, callback)
-        
-        req.end();
-    })
 
-const getWeather = new Promise (async(resolve)=>{
-    let data1 = await getCoordinates;
-        const {lat, lon} = data1[0];
+        req.end();
+    });
+}
+
+
+
+const getWeather = () => {
+
+    return new Promise(async (resolve) => {
+        let data1 = await getCoordinates(cityName());
+        const { lat, lon } = data1[0];
         console.log(lat, lon)
 
-    const callback = (res) => {
-        //console.log(res);
-        /// BIND EVENT HANDLERS
-        let chunks = '';
+        const callback = (res) => {
+            //console.log(res);
+            /// BIND EVENT HANDLERS
+            let chunks = '';
 
-        res.on('end', ()=> {
-            console.log('API response ended');
-            console.log(JSON.parse(chunks))
-            let {main:{temp_min, temp_max},wind:{speed} } = JSON.parse(chunks);
-            resolve({temp_min, temp_max, speed});
-        })
-    
-        res.on('data', (chunk)=> {
-            
-             chunks += chunk.toString();
-                console.log('API response with data'); 
-            
-        })
-    
-        res.on('error', ()=> {
-            console.log('API response with error');
-            
-    })
-    }
-         
-    // //preparerequest 
-    const req = http.request({
-        host: data.HOST,
-        path: data.PATH1 + `?lat=${lat.toFixed(2)}&lon=${lon.toFixed(2)}&appid=${data.KEY}`, 
-        method: 'GET',
-        port: 80
-    }, callback)
-    
-    req.end();
+            res.on('end', () => {
+                console.log('API response ended');
+                console.log(JSON.parse(chunks))
+                let { main: { temp_min, temp_max }, wind: { speed }, name } = JSON.parse(chunks);
+                resolve({ temp_min, temp_max, speed, name });
+            })
 
-    })
+            res.on('data', (chunk) => {
 
-    
+                chunks += chunk.toString();
+                console.log('API response with data');
 
+            })
 
+            res.on('error', () => {
+                console.log('API response with error');
 
+            })
+        }
 
+        // //preparerequest 
+        const req = http.request({
+            host: data.HOST,
+            path: data.PATH1 + `?lat=${lat.toFixed(2)}&lon=${lon.toFixed(2)}&appid=${data.KEY}`,
+            method: 'GET',
+            port: 80
+        }, callback)
 
+        req.end();
 
-let weatherData = await getWeather;   
-console.log({weatherData});
+    });
+}
 
+//export let weatherDisplayed;
 
-console.log(weatherData);
+const displayCurrentCityWeather = async () => {
+
+    const weatherData = await getWeather();
+
+    console.log(`The current weather in ${weatherData.name} F is:\n`);
+    console.log(`>>> min temperature ${weatherData.temp_min} F\n`)
+    console.log(`>>> max tempetrature ${weatherData.temp_max} F\n`)
+    console.log(`>>> wind speed ${weatherData.speed} m\s\n`)
+    weatherDisplayed = true;
+    // cityName();
+    //    return  weatherDataWasDisplayed = true;
+    // if weatherDisopayed = true Function to get intput
+}
+
+rl.question(
+    displayCurrentCityWeather()
+
+);
+
 
 
 //does this send the request?
 //1) node request build in
-//2) node -> npm 
+//2) node -> npm
 //3) OS related (linux)
 
 //4) REQUEST LIFECYCLE
--- readline1.js
+++ readline1.js
@@ -1,8 +1,21 @@
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
+//import { weatherDisplayed } from './app.js';
+//import { displayCurrentCityWeather } from './app.js';
 
 const rl = readline.createInterface({ input, output });
 
+let weatherDisplayed;
 
- export  const cityName = await rl.question(`Enter a city name >> `);
+export let cityName = async () => {
+    if (weatherDisplayed === true) {
+        await rl.question(`Enter a city name >> `);
+        return weatherDisplayed = false;
+    }
+}
 
+export { weatherDisplayed };
+
+// rl.on('line', (cityName) => {
+//     displayCurrentCityWeather();
+// }); 
